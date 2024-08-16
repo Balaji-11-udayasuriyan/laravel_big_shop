ec9cc3b2971f02fa6f4bc95eb8bb31fa2d22c674
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-08-16 18:42:31 +0530

    banner template added

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -34,6 +34,12 @@
     <!-- Include the feature categories partial -->
     @include('frontend.slider.feature_categories.type1')
 
+     <!-- Include the banner partial -->
+     @include('frontend.slider.banner.type1')
+
+     <!-- Include the popular product partial -->
+     @include('frontend.slider.popular_product.type1')
+
     <!-- Include the Search partial -->
     <!-- @include('frontend.layout.search') -->
 
