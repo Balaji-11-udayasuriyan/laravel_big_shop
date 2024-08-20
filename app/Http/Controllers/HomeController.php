6a2ac9863fa43f04566f57d8a81e21ca65bf135f
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-08-20 11:30:58 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,63 +16,6 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-
-        $categories= Category::all();
-
-        $query = Product::query();
-
-        // Apply filters based on the request
-        if ($request->has('category') && $request->category != 'All') {
-            $query->where('category_id', $request->category);
-        }
-
-        if ($request->has('search')) {
-            $searchTerm = $request->input('search');
-            $query->where('name', 'like', "%{$searchTerm}%");
-        }
-
-        $data = [
-            'product' => $query->get(),
-            'categories' => $categories
-        ];
-
-        return view('frontend/home', $data);
-    }
-
-    public function show($id)
-    {
-        try {
-            $product = Product::findOrFail($id);
-            return view('frontend.show', compact('product'));
-        } catch (ModelNotFoundException $e) {
-            // Handle the case where the product is not found
-            return redirect()->route('products.index')->with('error', 'Product not found.');
-        }
-    }
-        
-
-    public function sendEmailManually()
-    {
-        $recipient = 'vprabhu1771@gmail.com'; // Replace with actual recipient's email
-        $subject = 'Custom Subject';
-        $body = 'This is the body of the email. You can include HTML here if needed.';
-
-        Mail::raw($body, function(Message $message) use ($recipient, $subject) {
-            $message->to($recipient);
-            $message->subject($subject);
-            // You can add attachments or other options here if needed
-        });
-
-        return "Email sent successfully!";
-    }
-
-    public function login()
-    {
-        return view('frontend/auth/login');
-    }
-
-    public function register()
-    {
-        return view('frontend/auth/register');
+   
     }
 }
