9ef8b46fd69e3eeffe90efa27b03d55306e34294
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-08-21 12:54:51 +0530

    create State model and migration

---
+++ app/Models/State.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class State extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+
+        'country_id',
+        'name',
+        'code'
+    ];
+
+    public function country()
+    {
+        return this->belongsTo(Country::class, 'country_id');
+    }
+}
