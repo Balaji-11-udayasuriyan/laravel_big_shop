0095e17f9a71a4333589f6f22480abb8ae40bc3e
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-08-27 20:09:31 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,28 +11,35 @@ use Illuminate\Support\Facades\Storage;
 class Product extends Model
 {
     use HasFactory;
-    
     protected $table = 'products';
     
     protected $fillable = [
         'name',
+        'image_path',
         'price',
         'category_id',
         'brand_id',
-        'product_label_id',
         'qty',
-        'id',
         'alert_stock',
-        'image_path',
         'description',
-        
+        'product_label_id',
+        'product_tag_id',
+        'stock_status',
+        'is_featured',
+        'min_order_qty',
+        'max_order_qty'
     ];
 
     protected $casts = [
         'image_path' => 'array',
     ];
 
-    // public function category(): BelongsTo
+    public function brand():BelongsTo
+    {
+        return $this->belongsTo(Brand::class,'brand_id');
+    }
+
+    // public function category():BelongsTo
     // {
     //     return $this->belongsTo(Category::class,'category_id');
     // }
@@ -42,15 +49,9 @@ class Product extends Model
         return $this->belongsToMany(Category::class, 'product_categories');
     }
 
-
-    public function brand(): BelongsTo
-    {
-        return $this->belongsTo(Brand::class,'brand_id');
-    }
-
     public function productLabel()
     {
-        return $this->belongsToMany(Label::class,'product_labels');
+        return $this->belongsToMany(Label::class, 'product_labels');
     }
 
     public function productTag()
@@ -62,8 +63,12 @@ class Product extends Model
     {
         return $this->belongsToMany(Collection::class,'product_collections');
     }
-
     
+    public function productCompany()
+    {
+        return $this->belongsToMany(Company::class,'product_companies');
+    }
+
 
     public function GetImagePath()
     {
@@ -73,5 +78,13 @@ class Product extends Model
             }, $this->image_path);
         }
         return env('DOMAIN_URL') . Storage::url($this->image_path);
+    }
+
+    public function DeleteLogoImage()
+    {        
+        if (Storage::exists($this->image_path)) 
+        {
+            Storage::delete($this->image_path);
+        }
     }
 }
